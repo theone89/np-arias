a8d0d5cbdd235ae30ec0b42e5a4f2ce351ffd20d
theone8907 <franklincampos8907@gmail.com>
2024-08-24 19:09:29 -0400

    init

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,14 @@
+import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 
-export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = "es";
+// Can be imported from a shared config
+const locales = ["en", "es"];
+
+export default getRequestConfig(async ({ locale }) => {
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) notFound();
 
   return {
-    locale,
     messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
